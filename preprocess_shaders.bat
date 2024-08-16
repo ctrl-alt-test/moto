9fc82b2283f7cb999c7deb9153919368cbabae45
Julien Guertault <9511025+virtualzavie@users.noreply.github.com>
2024-08-16 02:42:20 +0200

    Move call to ShaderMinifier inside the preprocess_shaders script

--- preprocess_shaders.bat
+++ preprocess_shaders.bat
@@ -1,2 +1,6 @@
+pushd %~dp0
 cd src\shaders
 ..\..\ShaderPreprocessor.exe scene.frag
+popd
+
+shader_minifier.exe -v -o src\shaders\shaders.inl src\shaders\preprocessed.scene.frag src\shaders\scene.vert src\shaders\fxaa.frag
